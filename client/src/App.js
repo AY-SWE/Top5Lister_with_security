b516548edd10d481cb9b9c6e0c9dabe47ab6b88a
Andy Yang <yangandy1001@gmail.com>
2021-11-10 14:57:31 -0500

    post Bblackboard submission, redid initials

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import { React } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { AuthContextProvider } from './auth';
-import { GlobalStoreContextProvider } from './store'
+import "./App.css";
+import { React } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AuthContextProvider } from "./auth";
+import { GlobalStoreContextProvider } from "./store";
 import {
-    AppBanner,
-    HomeWrapper,
-    RegisterScreen,
-    Statusbar,
-    WorkspaceScreen
-} from './components'
-import LoginScreen from './components/LoginScreen';
+  AppBanner,
+  HomeWrapper,
+  RegisterScreen,
+  Statusbar,
+  WorkspaceScreen,
+} from "./components";
+import LoginScreen from "./components/LoginScreen";
 /*
     This is our application's top-level component.
     
@@ -23,22 +23,23 @@ import LoginScreen from './components/LoginScreen';
   @author McKilla Gorilla
 */
 const App = () => {
-    return (
-        <BrowserRouter>
-            <AuthContextProvider>
-                <GlobalStoreContextProvider>              
-                    <AppBanner />
-                    <Switch>
-                        <Route path="/" exact component={HomeWrapper} />
-                        <Route path="/login/" exact component={LoginScreen} />
-                        <Route path="/register/" exact component={RegisterScreen} />
-                        <Route path="/top5list/:id" exact component={WorkspaceScreen} />
-                    </Switch>
-                    <Statusbar />
-                </GlobalStoreContextProvider>
-            </AuthContextProvider>
-        </BrowserRouter>
-    )
-}
+  //the order below is important, the inner ones can access the outer ones
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <GlobalStoreContextProvider>
+          <AppBanner />
+          <Switch>
+            <Route path="/" exact component={HomeWrapper} />
+            <Route path="/login/" exact component={LoginScreen} />
+            <Route path="/register/" exact component={RegisterScreen} />
+            <Route path="/top5list/:id" exact component={WorkspaceScreen} />
+          </Switch>
+          <Statusbar />
+        </GlobalStoreContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,142 +1,152 @@
 import React, { createContext, useEffect, useState } from "react";
-import { useHistory } from 'react-router-dom'
-import api from '../api'
-
+import { useHistory } from "react-router-dom";
+import api from "../api";
 
 const AuthContext = createContext();
 console.log("create AuthContext: " + AuthContext);
 
 // THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
 export const AuthActionType = {
-    GET_LOGGED_IN: "GET_LOGGED_IN",
-    GET_LOGGED_OUT: "GET_LOGGED_OUT",
-    REGISTER_USER: "REGISTER_USER",
-    LOGIN_USER: "LOGIN_USER"
-}
+  GET_LOGGED_IN: "GET_LOGGED_IN",
+  GET_LOGGED_OUT: "GET_LOGGED_OUT",
+  REGISTER_USER: "REGISTER_USER",
+  LOGIN_USER: "LOGIN_USER",
+};
 
 function AuthContextProvider(props) {
-    const [auth, setAuth] = useState({
-        user: null,
-        loggedIn: false
-    });
-    const history = useHistory();
+  const [auth, setAuth] = useState({
+    user: null,
+    loggedIn: false,
+  });
+  const history = useHistory();
 
-    useEffect(() => {
-        auth.getLoggedIn();
-    }, []);
+  useEffect(() => {
+    auth.getLoggedIn();
+  }, []);
 
-    const authReducer = (action) => {
-        const { type, payload } = action;
-        switch (type) {
-            case AuthActionType.GET_LOGGED_IN: {
-                return setAuth({
-                    user: payload.user,
-                    loggedIn: payload.loggedIn
-                });
-            }
-            case AuthActionType.GET_LOGGED_OUT: {
-                return setAuth({
-                    user: payload.user,
-                    loggedIn: payload.loggedIn
-                })
-            }
-            case AuthActionType.REGISTER_USER: {
-                return setAuth({
-                    user: payload.user,
-                    loggedIn: true
-                })
-            }
-            case AuthActionType.LOGIN_USER: {
-                return setAuth({
-                    user: payload.user,
-                    loggedIn: true
-                })
-            }
-            
-            default:
-                return auth;
-        }
+  const authReducer = (action) => {
+    const { type, payload } = action;
+    switch (type) {
+      case AuthActionType.GET_LOGGED_IN: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: payload.loggedIn,
+        });
+      }
+      case AuthActionType.GET_LOGGED_OUT: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: payload.loggedIn,
+        });
+      }
+      case AuthActionType.REGISTER_USER: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: true,
+        });
+      }
+      case AuthActionType.LOGIN_USER: {
+        return setAuth({
+          user: payload.user,
+          loggedIn: true,
+        });
+      }
+
+      default:
+        return auth;
     }
+  };
 
-    auth.getLoggedIn = async function () {
-        try {
-            const response = await api.getLoggedIn();
-            if (response.status === 200) {
-            authReducer({
-                type: AuthActionType.GET_LOGGED_IN,
-                payload: {
-                    loggedIn: response.data.loggedIn,
-                    user: response.data.user
-                }
-            });
-            }
-        } catch (error) {
-            console.log("getLoggedIn error message is: "+ error.response.data.errorMessage);
-            
-        }
+  auth.getLoggedIn = async function () {
+    try {
+      const response = await api.getLoggedIn();
+      if (response.status === 200) {
+        authReducer({
+          type: AuthActionType.GET_LOGGED_IN,
+          payload: {
+            loggedIn: response.data.loggedIn,
+            user: response.data.user,
+          },
+        });
+      }
+    } catch (error) {
+      console.log(
+        "getLoggedIn error message is: " + error.response.data.errorMessage
+      );
     }
+  };
 
-    auth.logoutUser = async function(){
-        const response = await api.logoutUser();  
-        if(response.status === 200){
-            authReducer({
-                type: AuthActionType.GET_LOGGED_OUT,
-                payload: {
-                    loggedIn: response.data.loggedIn,   //is false
-                    user: response.data.user   //is null
-                }
-            });
-        }
+  auth.logoutUser = async function () {
+    const response = await api.logoutUser();
+    if (response.status === 200) {
+      authReducer({
+        type: AuthActionType.GET_LOGGED_OUT,
+        payload: {
+          loggedIn: response.data.loggedIn, //is false
+          user: response.data.user, //is null
+        },
+      });
     }
+  };
 
-    auth.registerUser = async function(userData, store) {
-        try {
-            const response = await api.registerUser(userData);      
-            if (response.status === 200) {
-                authReducer({
-                type: AuthActionType.REGISTER_USER,
-                payload: {
-                    user: response.data.user
-                }
-            })
-            history.push("/");
-            }
-            
-        } catch (error) {
-            console.log(error.response.data.errorMessage);
-            store.showAccountErrorModal();
-        }
+  auth.getUserInitials = function () {
+    let initials = "";
+    if (auth.user) {
+      initials += auth.user.firstName.charAt(0);
+      initials += auth.user.lastName.charAt(0);
     }
+    console.log("user initials: " + initials);
+    return initials;
+  };
 
-    auth.loginUser = async function(userData, store) {
-        //console.log(userData);
-        try {
-            const response = await api.loginUser(userData);      
-            if (response.status === 200) {
-                console.log(response.data.user);
-                authReducer({
-                    type: AuthActionType.LOGIN_USER,
-                    payload: {
-                        user: response.data.user
-                    }
-                })
-                history.push("/");
-                store.loadIdNamePairs();
-            }
-        } catch (error) {
-            console.log(error.response.data.errorMessage);
-            store.showAccountErrorModal();
-        }
+  auth.registerUser = async function (userData, store) {
+    try {
+      const response = await api.registerUser(userData);
+      if (response.status === 200) {
+        authReducer({
+          type: AuthActionType.REGISTER_USER,
+          payload: {
+            user: response.data.user,
+          },
+        });
+        history.push("/");
+      }
+    } catch (error) {
+      console.log(error.response.data.errorMessage);
+      store.showAccountErrorModal();
     }
+  };
 
+  auth.loginUser = async function (userData, store) {
+    //console.log(userData);
+    try {
+      const response = await api.loginUser(userData);
+      if (response.status === 200) {
+        console.log(response.data.user);
+        authReducer({
+          type: AuthActionType.LOGIN_USER,
+          payload: {
+            user: response.data.user,
+          },
+        });
+        history.push("/");
+        store.loadIdNamePairs();
+      }
+    } catch (error) {
+      console.log(error.response.data.errorMessage);
+      store.showAccountErrorModal();
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={{
-            auth
-        }}>
-            {props.children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthContext;
