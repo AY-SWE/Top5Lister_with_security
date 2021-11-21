e1056574d606ce627e7fd9a369ca95adc4d4026f
Andy Yang <yangandy1001@gmail.com>
2021-11-21 16:15:19 -0500

    finished the look of navigation bar, functions still imperfect

--- client/src/components/NavigateBar.js
+++ client/src/components/NavigateBar.js
@@ -2,7 +2,6 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../auth";
 import { GlobalStoreContext } from "../store";
-import EditToolbar from "./EditToolbar";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -11,6 +10,14 @@ import Typography from "@mui/material/Typography";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+
+import TextField from "@mui/material/TextField";
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import GroupsOutlinedIcon from "@mui/icons-material/GroupsOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FunctionsOutlinedIcon from "@mui/icons-material/FunctionsOutlined";
+import SortOutlinedIcon from "@mui/icons-material/SortOutlined";
 
 import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
@@ -25,35 +32,34 @@ export default function NavigateBar() {
 
   const history = useHistory();
 
-  const handleProfileMenuOpen = (event) => {
+  const handleSortByMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMenuClose = () => {
+  const handleSortByMenuClose = () => {
     setAnchorEl(null);
   };
 
-  const handleLogout = () => {
-    handleMenuClose();
-    auth.logoutUser();
-  };
-
-  function handleWelcomeLogin() {
-    history.push("/login/");
+  function handleKeyPress(event) {
+    // if (event.code === "Enter") {
+    //   let id = event.target.id.substring("list-".length);
+    //   store.changeListName(id, text);
+    //   toggleEdit();
+    // }
+    console.log("handleKeyPressSearchBar");
   }
-  function handleWelcomeRegister() {
-    history.push("/register/");
-  }
-  function handleWelcomeGuestLogin() {
-    history.push("/login/");
+  function handleUpdateText(event) {
+    //setText(event.target.value);
+    console.log("handleUpdateTextSearchBar");
   }
 
-  const menuId = "primary-search-account-menu";
-  const loggedOutMenu = (
+  const menuId = "sortby-menu";
+
+  const sortByMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: "top",
+        vertical: "bottom",
         horizontal: "right",
       }}
       id={menuId}
@@ -63,44 +69,21 @@ export default function NavigateBar() {
         horizontal: "right",
       }}
       open={isMenuOpen}
-      onClose={handleMenuClose}
+      onClose={handleSortByMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>
-        <Link to="/login/">Login</Link>
+      <MenuItem onClick={handlePublishDateNewest}>
+        Publish Date (Newest)
       </MenuItem>
-      <MenuItem onClick={handleMenuClose}>
-        <Link to="/register/">Create New Account</Link>
+      <MenuItem onClick={handlePublishDateOldest}>
+        Publish Date (Oldest)
       </MenuItem>
-    </Menu>
-  );
-  const loggedInMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      <MenuItem onClick={handleViews}>Views</MenuItem>
+      <MenuItem onClick={handleLikesMenuItem}>Likes</MenuItem>
+      <MenuItem onClick={handleDislikesMenuItem}>Dislikes</MenuItem>
     </Menu>
   );
 
-  let editToolbar = "";
-  let menu = loggedOutMenu;
-  if (auth.loggedIn) {
-    menu = loggedInMenu;
-    if (store.currentList) {
-      editToolbar = <EditToolbar />;
-    }
-  }
+  let menu = sortByMenu;
 
   let location = useLocation();
 
@@ -112,83 +95,109 @@ export default function NavigateBar() {
 
   // }
 
-  function getAccountMenu(loggedIn) {
-    let userInitials = auth.getUserInitials();
-    console.log("userInitials: " + userInitials);
-    if (loggedIn) return <div>{userInitials}</div>;
-    else return <AccountCircle />;
+  function handleHome() {
+    history.push("/login/");
   }
-  if (location.pathname == "/" && !auth.loggedIn) {
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" style={{ background: "#a19e9e" }}>
-          <Toolbar>
-            <Typography
-              variant="h4"
-              noWrap
-              component="div"
-              sx={{ display: { xs: "none", sm: "block" } }}
-            >
-              <Link style={{ textDecoration: "none", color: "gold" }} to="/">
-                T<sup>5</sup>L
-              </Link>
+  function handleAllLists() {
+    history.push("/login/");
+  }
+  function handleUsers() {
+    history.push("/login/");
+  }
+  function handleCommunity() {
+    history.push("/login/");
+  }
+
+  function handlePublishDateNewest() {
+    history.push("/login/");
+  }
+  function handlePublishDateOldest() {
+    history.push("/login/");
+  }
+  function handleViews() {
+    history.push("/login/");
+  }
+  function handleLikesMenuItem() {
+    history.push("/login/");
+  }
+  function handleDislikesMenuItem() {
+    history.push("/login/");
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ background: "#a19e9e" }}>
+        <Toolbar>
+          <Box sx={{ width: 280 }}>
+            <Button style={{ color: "black" }} onClick={handleHome}>
+              <HomeOutlinedIcon
+                fontSize="large"
+                aria-label="home"
+                id="home-list-button"
+              />
+            </Button>
+
+            <Button style={{ color: "black" }} onClick={handleAllLists}>
+              <GroupsOutlinedIcon
+                fontSize="large"
+                aria-label="allLists"
+                id="all-list-button"
+              />
+            </Button>
+
+            <Button style={{ color: "black" }} onClick={handleUsers}>
+              <PersonOutlineOutlinedIcon
+                fontSize="large"
+                aria-label="users"
+                id="users-list-button"
+              />
+            </Button>
+            <Button style={{ color: "black" }} onClick={handleCommunity}>
+              <FunctionsOutlinedIcon
+                fontSize="large"
+                aria-label="community"
+                id="community-list-button"
+              />
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <TextField
+              style={{ width: 350 }}
+              required
+              fullWidth
+              id="searchBar"
+              name="name"
+              className="search"
+              onKeyPress={handleKeyPress}
+              onChange={handleUpdateText}
+              placeholder="Search"
+              inputProps={{ style: { fontSize: 13 } }}
+              autoFocus
+              sx={{ background: "white" }}
+            />
+          </Box>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Typography sx={{ color: "black" }} variant="button">
+              Sort By
             </Typography>
-            <Box sx={{ flexGrow: 1 }}>{editToolbar}</Box>
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              <Button style={{ color: "#850a96" }} onClick={handleWelcomeLogin}>
-                Login
-              </Button>
-              <Button
-                style={{ color: "#850a96" }}
-                onClick={handleWelcomeRegister}
-              >
-                Create Account
-              </Button>
-              <Button
-                style={{ color: "#850a96" }}
-                onClick={handleWelcomeGuestLogin}
-              >
-                Continue As Guest
-              </Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        {menu}
-      </Box>
-    );
-  } else {
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" style={{ background: "#a19e9e" }}>
-          <Toolbar>
-            <Typography
-              variant="h4"
-              noWrap
-              component="div"
-              sx={{ display: { xs: "none", sm: "block" } }}
-            >
-              <Link style={{ textDecoration: "none", color: "gold" }} to="/">
-                T<sup>5</sup>L
-              </Link>
-            </Typography>
-            <Box sx={{ flexGrow: 1 }}>{editToolbar}</Box>
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
+          </Box>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button style={{ color: "black" }} onClick={handleSortByMenuOpen}>
+              <SortOutlinedIcon
+                fontSize="large"
+                aria-label="sortBy"
                 aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                {getAccountMenu(auth.loggedIn)}
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        {menu}
-      </Box>
-    );
-  }
+                aria-controls={menuId}
+                id="sortBy-list-button"
+              />
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {menu}
+    </Box>
+  );
 }
